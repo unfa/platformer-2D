f1cbcf8101412d3269468c11bcfdbd28dcb5573e
unfa <unfa@mail.unfamusic.com>
2015-06-03 18:14:07 +0200

    basic player control: walking left/right

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,30 @@ extends RigidBody2D
 # var a=2
 # var b="textvar"
 
-export var walk_speed = 150
+# EXPORT
+
+export var walk_speed = 225
+
+# INPUT
+
+var player_right = Input.is_action_pressed("player_right")
+var player_left = Input.is_action_pressed("player_left")
+var player_jump = Input.is_action_pressed("player_jump")
 
 func _ready():
 	# Initialization here
 	set_fixed_process(true)
 
 func _fixed_process(delta):
-	set_linear_velocity(Vector2(self.walk_speed,get_linear_velocity().y))
+	# refreshing INPUT
+	player_right = Input.is_action_pressed("player_right")
+	player_left = Input.is_action_pressed("player_left")
+	player_jump = Input.is_action_pressed("player_jump")
+
+	if player_right:
+		set_linear_velocity(Vector2(self.walk_speed,get_linear_velocity().y))
+	
+	elif player_left:
+		set_linear_velocity(Vector2(self.walk_speed*(-1),get_linear_velocity().y))
+	else:
+		set_linear_velocity(Vector2(0,get_linear_velocity().y))
